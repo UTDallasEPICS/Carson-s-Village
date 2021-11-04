418b21436b1c79bb353b7b1ab4ac4da967535f1e
Marlon Schumacher <mws180000@utdallas.edu>
2021-11-03 19:46:22 -0500

    Changed database password and made database.js read credentials from .env file

--- CarsonsVillage_Website-Upgrade-main/CarsonsVillage_Website-Upgrade-main/node v2/database.js
+++ CarsonsVillage_Website-Upgrade-main/CarsonsVillage_Website-Upgrade-main/node v2/database.js
@@ -7,18 +7,21 @@
 *	WARNING: DO NOT DIRECTLY DECLARE CREDENTIALS IN FINAL PRODUCT, PROTOTYPE/DEMONSTRATION USE ONLY
 *		in final product, pass credentials as environment variables for security reasons
 */
+const dotenv = require('dotenv');
+dotenv.config();
 const {Client} = require('pg');
 /*
 *	declare PostgreSQL database credentials to initiate connection
 *	the below represents default user, host, password, and port for a PostgresSQL table
 *		for security reasons, set these to a different value in the final product
 */
+console.log(`${process.env.DB_PASSWORD}`);
 const client = new Client({
-	user: 'edvtjzpq', 
-    host: 'fanny.db.elephantsql.com', 
-    database: 'edvtjzpq', 
-    password: 'WnwdnhLpZXu_6pidJHSoD9vTwg-ZAsfG', 
-    port: 5432
+	user: process.env.DB_USER, 
+    host: process.env.DB_HOST, 
+    database: process.env.DATABASE, 
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_PORT
 });
 //export modules for user in server.js
 module.exports = client;
