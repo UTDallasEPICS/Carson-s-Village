9e09c4a806a0d6d5a6342b86ea362f63126ab241
pariah <pariah@pop-os.localdomain>
2023-04-30 18:52:14 -0500

    merged changes

--- app.vue
+++ app.vue
@@ -4,11 +4,11 @@
 .group.flex.flex-row
   NuxtLink.items-center.px-2.py-2.text-base.font-medium.rounded-md.text-green-600.cursor-pointer(
   class='hover:text-white hover:bg-gray-600' 
-  v-for="(route, i) in routes" 
-  :key="i" 
-  :to="route.path"
+    v-for="(route, i) in routes" 
+    :key="i" 
+    :to="route.path"
   ) 
-  p.uppercase.white {{route.name}}
+    p.uppercase.white {{route.name}}
 p the above is an example of how to work with routes, which are what govern navigation in Nuxt. 
 p Keep in mind that public users (those who are not logged in) should *not* see a nav - they just need search and page view.
 NuxtLink.items-center.px-2.py-2.text-base.font-medium.rounded-md.text-green-600.cursor-pointer(
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,12 @@
 type Profile = {
   last_name: string,
   first_name: string,
-  role: string,
+  user_role: string,
   email: string,
   phone: string; 
 };
-const cvuser = useCookie('cvuser');
-const user = computed<Profile>(() => JSON.parse(cvuser.value || "{}"))
-const isAdmin = computed(() => user.value.role == "admin")
+const cvuser = useCookie<Profile>('cvuser');
+const isAdmin = computed(() => cvuser.value?.user_role == "advocate")
 </script>
 
 <template lang="pug">
@@ -18,14 +17,14 @@ div
         .text-white.bg-blue.w-full.m-0.text-3xl.font-poppins.font-bold.text-center.drop-shadow-sm.p-10(v-else class="sm:text-5xl" style="text-shadow: 3px 3px 4px rgba(0, 0, 0, 0.25);") Family Profile
         .flex.flex-col.justify-start.gap-6(class="sm:grid sm:gap-10 sm:grid-rows-3 sm:grid-cols-2 sm:my-16")
             .text-gray-dark.font-poppins.font-bold.pt-4.text-xl.ml-8(class="sm:mx-auto sm:text-3xl") Name:
-            .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:text-3xl") {{ user.first_name }} {{ user.last_name }}
+            .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:text-3xl") {{ cvuser.first_name }} {{ cvuser.last_name }}
             .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:mx-auto sm:text-3xl")   Email:
-            .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:text-3xl") {{user.email}}
+            .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:text-3xl") {{cvuser.email}}
             .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:mx-auto sm:text-3xl")   Phone:
-            .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:text-3xl") {{user.phone}}
+            .text-gray-dark.font-poppins.font-bold.text-xl.ml-8(class="sm:text-3xl") {{cvuser.phone}}
         .buttons.flex.gap-3(class=" sm:gap-10 space-between  sm:my-24" )
             NuxtLink.p-4.mx-auto.text-white.font-poppins.font-bold.bg-orange-400.rounded-full.w-fit(to='/Pages') List of client pages
-            NuxtLink.p-4.mx-auto.text-white.font-poppins.font-bold.bg-orange-400.rounded-full.w-fit(to='EditPage/0') Insert new page
+            NuxtLink.p-4.mx-auto.text-white.font-poppins.font-bold.bg-orange-400.rounded-full.w-fit(to='/EditPage/0') Insert new page
             NuxtLink.p-4.mx-auto.text-white.font-poppins.font-bold.bg-orange-400.rounded-full.w-fit(v-if="isAdmin" to='/Users') See all users
             NuxtLink.p-4.mx-auto.text-white.font-poppins.font-bold.bg-orange-400.rounded-full.w-fit(v-if="isAdmin" to='/EditUser/0') Invite user
 </template>
