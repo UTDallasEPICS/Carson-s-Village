d19ca2b175fdad3886a1de89d292a1982f4153fe
shawnstha <shalinstha@hotmail.com>
2023-05-07 16:10:56 -0500

    small ui changes

--- pages/Pages.vue
+++ pages/Pages.vue
@@ -36,11 +36,14 @@ const pageSearch = async() => {
 <template lang="pug">
 .container.mx-auto
   h1(class = 'text-2xl indent-8 mt-2') Memorial Page Search
+  
   br
   legend(class='text-m indent-8') Search memorial pages
   input(class="border border-gray-300 py-2 px-4 ml-8 rounded-lg focus:outline-none focus:border-black-500" type="search" placeholder=" " v-model="searchQuery")
   button(class='text-m bg-gray-300 p-2 mt-1 mb-2' @click='pageSearch') SEARCH
-  b(class="ml-8")     Search Results 
+  .container
+  br
+  b(class="ml-8 text-xl")     Search Results 
   table.table.table-striped(style="width:100%;")
       thead
         tr.bg-green-400.text-white
@@ -49,10 +52,10 @@ const pageSearch = async() => {
           th.px-8 Deadline
       tbody
         tr(v-for="page in pages")
-          td 
+          td(style="text-align: center")   
             NuxtLink(:to="`/Page/${page.cuid}`") {{ page.page_name}}
-          td {{ page.donation_goal }}
-          td {{ page.deadline }}
+          td(style="text-align: center") {{ page.donation_goal }}
+          td(style="text-align: center") {{ page.deadline }}
               
 </template>
 
