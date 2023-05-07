4221ebca811f435b114136eea563bd1a43b7ba41
shawnstha <shalinstha@hotmail.com>
2023-05-07 15:55:49 -0500

    Implemented page search functionality based on page names

--- pages/Pages.vue
+++ pages/Pages.vue
@@ -24,7 +24,7 @@ type Page ={
 }
 const pages = ref<Page[]>([])
 const searchQuery = ref('');
-const search = async() => { 
+const pageSearch = async() => { 
     const { data : pageData } = await useFetch('/api/pages', {
     method: 'GET',
     query: {searchQuery: searchQuery.value}    
@@ -39,7 +39,7 @@ const search = async() => {
   br
   legend(class='text-m indent-8') Search memorial pages
   input(class="border border-gray-300 py-2 px-4 ml-8 rounded-lg focus:outline-none focus:border-black-500" type="search" placeholder=" " v-model="searchQuery")
-  button(class='text-m bg-gray-300 p-2 mt-1 mb-2' @click='search') SEARCH
+  button(class='text-m bg-gray-300 p-2 mt-1 mb-2' @click='pageSearch') SEARCH
   b(class="ml-8")     Search Results 
   table.table.table-striped(style="width:100%;")
       thead
@@ -50,8 +50,8 @@ const search = async() => {
       tbody
         tr(v-for="page in pages")
           td 
-            NuxtLink(:to='/Page/`${page.cuid}`') {{ page.page_name}}
-          td {{ page.donation }}
+            NuxtLink(:to="`/Page/${page.cuid}`") {{ page.page_name}}
+          td {{ page.donation_goal }}
           td {{ page.deadline }}
               
 </template>
