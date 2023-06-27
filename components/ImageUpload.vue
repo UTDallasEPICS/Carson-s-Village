e51c5329680f99d2f1a8324f724587c6c3507653
pariah <pariah@pop-os.localdomain>
2023-06-27 11:47:25 -0500

    add some notes ofr image uploads

--- components/ImageUpload.vue
+++ components/ImageUpload.vue
@@ -1,85 +1,58 @@
 <script lang="ts" setup>
+import { Image } from '~~/types';
 
-const emit = defineEmits(['update:image-uploaded'])
-type imageObject = {
-  size: number,
-  contentType: string,
-
-}
+// TODO: this component needs to handle getting the presigned url (which should also create an Image entry in the database), performing the upload, and then emitting the Image object (not the file) so that
+// the parent component can add it to its list
+// when saving a page, we ignore the images array
+// page editor can select from currently uploaded images to pick the cuid of the one to use as profile image
+// we dont need model value because we arent binding to a variable, we are emitting an event with the data
+const emit = defineEmits(['imageUploaded']);
 
 type imageLinkTypes = {
   uploadUrl: string,
-  contentURl: string
+  image: Image
 }
-
-const imageLinks = ref<imageLinkTypes>({
-  uploadUrl: "",
-  contentURl: ""
-})
-
+// we need to know what page we are adding images to
 const props = defineProps({
-    modelValue: {
+    page: {
         type: String,
         default: "",
     }
 });
 
-const getSignedUrl = async (file: File) => {
-  // perform the request to get the presigned url from server (image_upload) and return it
-  // the api returns {uploadUrl, contentURl} from image_upload.post.ts
-  // upload url gets used to upload the file, the content URL is going to be used to 
-  // create an image entry in the database (This is a third api call to image.post.ts)
-  try {
-    const { data: imageData } = await useFetch('/api/image_upload', {
-        method: 'POST',
-        body: { size:file.size, contentType: file.type, file }
-      })
-      imageLinks.value = imageData as unknown as imageLinkTypes
-      return imageLinks // return {content-url,}
-    } catch (error) {
-      console.error(error)
-      throw new Error('Failed to get signed URL')
-  }
-}
-// actually uploads images using presigned url to s3 bucket
-const uploadFile = async (data: BodyInit, ContentLength:string, ContentType:string, presignedUrl:string, imageLink: string)  => {
-  const response = await fetch(presignedUrl, {
-        method: "PUT",
-        headers: {
-        "Content-Length": ContentLength,
-        "Content-Type": ContentType,
-      },
-        body: data,
-      }).then((res) => {
-      return res.json()
-      })
-      //const { uploadUrl, contentUrl } = data
-      //return { uploadUrl, contentUrl }
 
-      
-      if (!response.ok) {
-        throw new Error("Failed to upload data")
-      }
-      emit('update:image-uploaded', imageLink)
-  // do actual upload, after upload finished, emit imageURL*/
+// actually uploads images using presigned url to s3 bucket
+const onFile = async (event: Event) => {
+  const file = event?.target?.files[0];
+
+  // TODO: this needs to be modified so that the API call also creates an image in the database
+  const { data: imageData } = await useFetch('/api/image_upload', {
+    method: 'POST',
+    body: { size: file.size, contentType: file.type, file }
+  });
+  const { uploadUrl, image} = imageData as unknown as imageLinkTypes;
+  const response = await fetch(uploadUrl, {
+    method: "PUT",
+    headers: {
+    "Content-Length": file.size,
+    "Content-Type": file.type,
+  },
+    body: file,
+  })
+  if (!response.ok) {
+    throw new Error("Failed to upload data")
+  }
+  // TODO: parent component adds this image to its list of images
+  emit('imageUploaded', image)
   } 
 </script>
 
 
-<template lang ="pug">
-input#images.rounded-md.outline-0.border-box.p-2(class="sm:ml-2" style="border: 1px solid #c4c4c4;" type="file"  :value="modelValue" @input="$emit('update:modelValue',$event.target.value)" accept=".png,.jpeg,.jpg,.gif" multiple)
+<template lang="pug">
+input#images.rounded-md.outline-0.border-box.p-2(
+  class="sm:ml-2" 
+  style="border: 1px solid #c4c4c4;" 
+  type="file"  
+  @change="onFile"
+  accept=".png,.jpeg,.jpg,.gif" multiple)
 </template>
-
-<script lang = "ts">
-export default {
-  methods: {
-    handleFileUpload(event) {
-      const file = event.target?.files[0] as unknown as File;
-      //const presign = getSignedUrl(file)
-      //const upload = (async() => await uploadFile(file,file.size+"",file.type,(await presign).value.uploadUrl,(await presign).value.contentURl ))
-      console.log(`File type: ${file.type}`);
-      console.log(`File size: ${file.size} bytes`);
-    },
-  },
-};
-</script>
