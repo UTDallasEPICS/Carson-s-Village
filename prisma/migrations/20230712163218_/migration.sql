a03e9b2a7bb0ae55622ffd9640e9e4cfe5966a4e
pariah <pariah@pop-os.localdomain>
2023-08-09 14:59:15 -0500

    user check

--- prisma/migrations/20230712163218_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `profile_image_cuid` to the `pages` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "pages" ADD COLUMN     "profile_image_cuid" TEXT NOT NULL;
