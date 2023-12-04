ff85cb65f11909d428f6186bee9b7e3ac6e9096f
Shalom Michael <shalommichael2000@gmail.com>
2023-12-04 15:18:58 -0600

    updated logic flow for transer enablement, connected accounts

--- server/api/family_onboarding.get.ts
+++ server/api/family_onboarding.get.ts
@@ -1,57 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-import Stripe from "stripe";
-import { getQuery } from 'h3';
 import { useRuntimeConfig } from '#imports';
+import { getQuery } from 'h3';
 
-const prisma = new PrismaClient();
 const runtime = useRuntimeConfig();
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripe = new Stripe(stripeSecretKey, { apiVersion: "2022-11-15" });
 
 export default defineEventHandler(async (event) => {
   const { familyCuid } = await getQuery(event);
 
   if (!familyCuid) {
-    // Family CUID not provided error handling
-    // return createErrorResponse(event, 'Family CUID is required');
-    // Uncomment above line to enable error handling
+    // Handle the error case where familyCuid is not provided, if necessary
+    // return { error: "Family CUID is required" };
   }
 
-  const family = await prisma.family.findUnique({
-    where: { cuid: familyCuid },
-    select: { Stripe_Account_id: true },
-  });
-
-  if (!family || !family.Stripe_Account_id) {
-    const newStripeAccount = await stripe.accounts.create({
-      type: 'standard',
-      email: event.context.user.email, // Ensure the user's email is available in the context
-    });
-
-    await prisma.family.update({
-      where: { cuid: familyCuid },
-      data: { Stripe_Account_id: newStripeAccount.id }
-    });
-
-    const accountLink = await stripe.accountLinks.create({
-      account: newStripeAccount.id,
-      refresh_url: `${runtime.public.BASE_URL}/api/family_onboarding.get?familyCuid=${familyCuid}`,
-      return_url: `${runtime.public.BASE_URL}/`,
-      type: 'account_onboarding',
-    });
-
-    return createRedirectResponse(event, accountLink.url);
-  } else {
-    // If the family already has a Stripe account, redirect to the home page
-    return createRedirectResponse(event, `${runtime.public.BASE_URL}/`);
-  }
+  return createRedirectResponse(event, `${runtime.BASEURL}/`);
 });
-
-// Commented out the error response function
-// function createErrorResponse(event, message) {
-//   event.res.statusCode = 400;
-//   return { error: message };
-// }
 
 function createRedirectResponse(event, location) {
   event.res.writeHead(302, { Location: location });
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,62 +1,70 @@
-import { PrismaClient } from "@prisma/client";
-import jwt from "jsonwebtoken";
-import fs from "fs";
+import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
 import { parseCookies } from 'h3';
-import Stripe from "stripe";
+import Stripe from 'stripe';
 import { useRuntimeConfig } from '#imports';
 
 const prisma = new PrismaClient();
 const runtime = useRuntimeConfig();
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: "2022-11-15" });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
 
 export default defineEventHandler(async (event) => {
   const cookies = parseCookies(event);
-  const cvtoken = cookies.cvtoken || "";
+  const cvtoken = cookies.cvtoken || '';
 
   if (!cvtoken && !event.req.url?.startsWith('/api/callback')) {
     return createRedirectResponse(event, `${runtime.BASEURL}/login`);
-  } else {
-    if (cvtoken) {
-      try {
-        const publicKey = fs.readFileSync(process.cwd() + "/cert-dev.pem", 'utf8');
-        const claims = jwt.verify(cvtoken, publicKey);
-        const user = await prisma.user.findUnique({
-          where: { email: claims.email },
-          include: {
-            Family: true
-          }
-        });
+  }
 
-        if (!user) {
-          return createRedirectResponse(event, `${runtime.BASEURL}/login`);
-        }
+  if (cvtoken) {
+    try {
+      const publicKey = fs.readFileSync(process.cwd() + '/cert-dev.pem', 'utf8');
+      const claims = jwt.verify(cvtoken, publicKey);
+      const user = await prisma.user.findUnique({
+        where: { email: claims.email },
+        include: { Family: true },
+      });
 
-        if (user.user_role === 'family' && !user.Family?.Stripe_Account_id) {
-          const newStripeAccount = await stripe.accounts.create({
-            type: 'standard',
-            email: user.email,
-          });
-
-          await prisma.family.update({
-            where: { cuid: user.Family.cuid },
-            data: { Stripe_Account_id: newStripeAccount.id }
-          });
-
-          const accountLink = await stripe.accountLinks.create({
-            account: newStripeAccount.id,
-            refresh_url: `${runtime.BASEURL}/api/family_onboarding.get?familyCuid=${user.Family.cuid}`,
-            return_url: `${runtime.BASEURL}/`, // Users will be redirected here after onboarding
-            type: 'account_onboarding',
-          });
-
-          return createRedirectResponse(event, accountLink.url);
-        }
-
-        // If the user has a Stripe account or is not part of a family, proceed as normal
-      } catch (error) {
-        console.error(error);
+      if (!user) {
         return createRedirectResponse(event, `${runtime.BASEURL}/login`);
       }
+
+      if (user.user_role === 'family' && !user.Family?.Stripe_Account_id) {
+        const customAccountData = {
+          email: user.email,
+          type: 'custom',
+          business_type: 'individual',
+          requested_capabilities: ['transfers'],
+          // Include other necessary fields as per Stripe and Carson Village requirements for custom accounts
+        };
+
+        const newStripeAccount = await stripe.accounts.create(customAccountData);
+
+        await prisma.family.update({
+          where: { cuid: user.Family.cuid },
+          data: { Stripe_Account_id: newStripeAccount.id },
+        });
+
+        const accountLink = await stripe.accountLinks.create({
+          account: newStripeAccount.id,
+          refresh_url: `${runtime.BASEURL}/api/family_onboarding.get?familyCuid=${user.Family.cuid}`,
+          return_url: `${runtime.BASEURL}/dashboard`, // Redirect to dashboard after onboarding.
+          type: 'account_onboarding',
+        });
+
+        return createRedirectResponse(event, accountLink.url);
+      } else if (user.user_role === 'family' && user.Family?.Stripe_Account_id) {
+        // Check the existing Stripe account for transfers capability status.
+        const stripeAccount = await stripe.accounts.retrieve(user.Family.Stripe_Account_id);
+        if (stripeAccount.capabilities.transfers === 'inactive') {
+          const additionalInfoUrl = `${runtime.BASEURL}/additional-info?familyCuid=${user.Family.cuid}`;
+          return createRedirectResponse(event, additionalInfoUrl);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      return createRedirectResponse(event, `${runtime.BASEURL}/login`);
     }
   }
 });
