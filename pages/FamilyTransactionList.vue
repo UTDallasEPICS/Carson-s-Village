94519e2442a2805ef290ceb5b209324524bdbfd8
Samuel Ma <fegelsamuel@gmail.com>
2024-02-20 16:16:06 -0600

    schema and prisma changes

--- pages/FamilyTransactionList.vue
+++ pages/FamilyTransactionList.vue
@@ -107,7 +107,7 @@ const isAdvocate = computed(() => cvuser.value?.user_role == "advocate")
             leave-to-class='opacity-0'
           )
             ListboxOptions(as='div' class='w-full absolute z-10 mt-10 bg-white shadow-lg max-h-60 rounded-md px-2 py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm' )
-              ListboxOption(as='div' v-for="page in familyData.raw_data" :key="page.cuid" :value="page.cuid" class="px-2 border border-grey-500 py-1 my-1") {{ page.page_name }} | {{ donationFormat(page?.amount_raised - page?.amount_distributed) }}
+              ListboxOption(as='div' v-for="page in familyData.raw_data" :key="page.cuid" :value="page.cuid" class="px-2 border border-grey-500 py-1 my-1") {{ page.first_name + " "  + page.last_name }} | {{ donationFormat(page?.amount_raised - page?.amount_distributed) }}
         ListboxButton(class='text-left bg-white relative rounded-md pl-2 pr-10 py-2 sm:text-sm w-96') {{ currentPageCuid ? currentPage.page_name : 'Select Page' }}
   
   .flex.gap-5.justify-around
@@ -151,7 +151,7 @@ const isAdvocate = computed(() => cvuser.value?.user_role == "advocate")
               :key="i" 
               :class="{'bg-gray-200': (i+1) % 2}"
           )
-              td.font-poppins.text-gray-dark.font-bold(style="text-align: center")  {{ item?.page_name }}
+              td.font-poppins.text-gray-dark.font-bold(style="text-align: center")  {{ item?.first_name + " " + item?.last_name }}
               td.font-poppins.text-gray-dark.font-bold(style="text-align: center")  {{ donationFormat(item.amount_raised) }}
               td.font-poppins.text-gray-dark.font-bold(style="text-align: center")  {{ donationFormat(item.amount_raised-item.amount_distributed) }}
               td.font-poppins.text-gray-dark.font-bold(style="text-align: center")  {{ item.cuid }}
