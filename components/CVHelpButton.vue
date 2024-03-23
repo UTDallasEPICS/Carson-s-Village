9e578a3a37423eeb662483d5e77db64bebb4eed4
Samuel Ma <fegelsamuel@gmail.com>
2024-03-23 17:52:15 -0500

    Help Button finished

---
+++ components/CVHelpButton.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.group.overflow-visible.relative
+  p(class="invisible group-hover:visible absolute bg-white w-64 p-4 shadow-lg") {{ description }}
+  svg(xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="group w-4 h-4 transition duration-300" :description="description")
+    path(stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z")
+</template>
+
+<script setup lang="ts">
+// properties are what's inside of the specific "Help Button" when you hover over it
+// Regarding the usage of .group, pug assumes you're making a div here when you do `.classname`
+const props = defineProps<{ description: string}>()
+</script>
