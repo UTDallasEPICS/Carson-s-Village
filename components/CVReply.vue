d9f4a7b94686aded96cb873841c4d67ce60c5bfb
FegelSamuel <fegelsamuel@gmail.com>
2024-04-19 19:59:33 -0500

    redid tabbing on component

--- components/CVReply.vue
+++ components/CVReply.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
 // I removed the `for i` stuff because I want to make an individual Reply component here and will have a for loop that displays everything as it is in where the component is being called
 div
-      div(v-if="modelValue.reply.length > 0", class="reply-box", style="padding: 1rem; text-align: left; border-bottom: 1px solid black")
-        div.reply-header(style="font-size: 1rem; font-weight: bold; margin-bottom: 2.5rem; margin-left: 1rem") {{ modelValue.name }}
-        div.reply-body(style="font-size: 1rem; color: #666; margin-bottom: 2.5rem;") {{ modelValue.reply }}
+  div(v-if="modelValue.reply.length > 0", class="reply-box", style="padding: 1rem; text-align: left; border-bottom: 1px solid black")
+    div.reply-header(style="font-size: 1rem; font-weight: bold; margin-bottom: 2.5rem; margin-left: 1rem") {{ modelValue.name }}
+    div.reply-body(style="font-size: 1rem; color: #666; margin-bottom: 2.5rem;") {{ modelValue.reply }}
 </template>
   
 <script setup lang="ts">
 import type {Reply} from '@/types.d.ts'
-const props = defineProps<{ modelValue: Reply}>()
+const props = defineProps<{ modelValue: Reply }>()
 </script>
