640f2bde015db783229947c77dd8700df08d19b4
Alonso Toji <User@MacBook-Air-3.fios-router.home>
2024-07-06 13:14:49 -0500

    work on dropdown menu and add menu hierarchy on pre-login page

--- components/CVHeader.vue
+++ components/CVHeader.vue
@@ -29,7 +29,7 @@ ClientOnly
   .max-w-min.mx-auto.flex.gap-2.mt-7(style="text-align:center")
     div.max-w-min.mx-auto.flex.gap-2(v-if="isLoggedIn")
       a.items-center.px-2.py-2.text-base.font-medium.rounded-md.text-gray-999.cursor-pointer(
-      class='hover:text-black border border-gray-999 hover:border-green-999 bg-white'  
+      class='hover:text-black bg-white'  
       href="/api/logout"
       ) 
         p.uppercase.white.w-max LOGOUT
@@ -53,27 +53,58 @@ ClientOnly
         p.uppercase.white.w-max Family Reports
     div.max-w-min.mx-auto.flex.gap-2(v-else)
       a.items-center.px-2.py-2.text-base.font-medium.rounded-md.text-gray-999.cursor-pointer(
-      class='hover:!text-black border border-gray-999 hover:border-green-999 bg-white'  
+      class='hover:!text-black bg-white'  
       href="/api/login"
       ) 
         p.uppercase.white.w-max LOGIN
+
       NavLinkButton(to='https://carsonsvillage.org/' target="_blank") 
         p.uppercase.white.w-max HOME
-      NavLinkButton(to='https://carsonsvillage.org/#' target="_blank") 
-        p.uppercase.white.w-max RESOURCES
-      NavLinkButton(to='https://carsonsvillage.org/get-involved/' target="_blank") 
+
+      DropdownMenu(:has-submenus="true" :num-submenus="4"
+        :submenus="[{ title: 'Timeline of Important Events', to: 'https://carsonsvillage.org/timeline-of-events/' }, { title: 'Resource Library', to: 'http://carsonsvillage.org/resource-library' }, { title: 'Group Support', to: 'https://carsonsvillage.org/grief-group-support/' }, { title: 'Find Support', to: 'https://carsonsvillage.org/support/' }]" :dropdownMinWidth="200")
+        | RESOURCES
+
+      NavLinkButton(to='https://carsonsvillage.org/get-involved/' target="_blank")
         p.uppercase.white.w-max GET INVOLVED
-      NavLinkButton(to='https://carsonsvillage.org/' target="_blank") 
-        p.uppercase.white.w-max ABOUT US
+
+      DropdownMenu(:has-submenus="true" :num-submenus="6"
+        :submenus="[{ title: 'Our Story', to: 'https://carsonsvillage.org/about-us/our-family/' }, { title: 'Our Testimonies', to: 'https://carsonsvillage.org/our-testimonials/' }, { title: 'In The News', to: 'https://carsonsvillage.org/about-us/in-the-news/' }, { title: 'Newsletter Archive', to: 'https://carsonsvillage.org/about-us/newsletter-archive/' }, { title: 'Our Team', submenus: [{ title: 'Advocates', to: 'https://carsonsvillage.org/about-us/advocates/' }, { title: 'Clinical Consultants', to: 'https://carsonsvillage.org/about-us/clinical-consultants/' }, { title: 'Support Team', to: 'https://carsonsvillage.org/about-us/meet-our-team/' }]}, { title: 'Board of Directors', to: 'https://carsonsvillage.org/about-us/board-of-directors/' }]" :dropdownMinWidth="150" :nestedDropdownMinWidth="200")
+        | ABOUT&nbsp;US 
     
     //&& isLoggedIn")
     .flex.w-max(v-if="isNotSearch")
       input(class="border border-gray-300 py-2 px-4 rounded-lg focus:outline-none focus:border-black-500" 
-      type="search" placeholder=" " v-model="searchQuery" v-on:keyup.enter="onEnter")
+      type="search" placeholder=" " v-model="searchQuery" v-on:keyup.enter="onEnter" style="height: 40px")
       NuxtLink.inline(:to="`/Search/?search=${searchQuery}&isPageList=0`")
-        img(src="/CVSearchIcon.png")
+        img(src="/CVSearchIcon.png" style="height: 40px")
     //.flex.w-max(v-else) Todo: add to search page
       p.uppercase.white.w-max Welcome TO Carson's Village
 </template>
 
-<style scoped></style>
+<style scoped>
+.no-border {
+  border: none;
+}
+
+.relative {
+  position: relative;
+}
+
+.button-content::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 2px;
+  height: 100%;
+  background-color: transparent;
+  transition: background-color 0.3s;
+}
+
+.no-border:hover .button-content::before,
+.active .button-content::before {
+  background-color: green;
+}
+</style>
---
+++ components/DropdownMenu.vue
@@ -0,0 +1,255 @@
+<template>
+  <div class="dropdown">
+    <NuxtLink
+      v-if="to && !submenus"
+      class="items-center px-2 py-2 text-base font-medium rounded-md cursor-pointer no-border relative"
+      :class="buttonClasses"
+      :to="to"
+      :target="target"
+      active-class="active"
+      @mouseover="showDropdown"
+      @mouseleave="hideDropdown"
+    >
+      <span class="button-content">
+        <slot />
+        <span class="caret" v-if="hasSubmenus">⌄</span>
+      </span>
+      <div
+        class="dropdown-content"
+        v-if="isDropdownVisible"
+        :style="{ minWidth: dropdownMinWidth + 'px' }"
+      >
+        <template v-for="(submenu, index) in submenus" :key="index">
+          <NuxtLink
+            v-if="submenu.to"
+            :to="submenu.to"
+            class="submenu-item"
+            @mouseover="highlightSubmenu(index)"
+            @mouseleave="resetHighlight"
+          >
+            {{ submenu.title }}
+          </NuxtLink>
+          <div
+            v-else
+            class="submenu-item"
+            @mouseover="highlightSubmenu(index)"
+            @mouseleave="resetHighlight"
+          >
+            {{ submenu.title }}
+            <div
+              class="nested-dropdown-content"
+              v-if="submenu.submenus && submenu.submenus.length > 0"
+              :style="{ minWidth: nestedDropdownMinWidth + 'px' }"
+              @mouseover="showNestedDropdown(index)"
+              @mouseleave="hideNestedDropdown(index)"
+            >
+              <template v-for="(nestedSubmenu, nestedIndex) in submenu.submenus" :key="nestedIndex">
+                <NuxtLink
+                  :to="nestedSubmenu.to"
+                  class="submenu-item"
+                >
+                  {{ nestedSubmenu.title }}
+                </NuxtLink>
+              </template>
+            </div>
+          </div>
+        </template>
+      </div>
+    </NuxtLink>
+    <div
+      v-else
+      class="items-center px-2 py-2 text-base font-medium rounded-md cursor-pointer no-border relative"
+      :class="buttonClasses"
+      @mouseover="showDropdown"
+      @mouseleave="hideDropdown"
+    >
+      <span class="button-content">
+        <slot />
+        <span class="caret" v-if="hasSubmenus">⌄</span>
+      </span>
+      <div
+        class="dropdown-content"
+        v-if="isDropdownVisible"
+        :style="{ minWidth: dropdownMinWidth + 'px' }"
+      >
+        <template v-for="(submenu, index) in submenus" :key="index">
+          <NuxtLink
+            v-if="submenu.to"
+            :to="submenu.to"
+            class="submenu-item"
+            @mouseover="highlightSubmenu(index)"
+            @mouseleave="resetHighlight"
+          >
+            {{ submenu.title }}
+          </NuxtLink>
+          <div
+            v-else
+            class="submenu-item"
+            @mouseover="highlightSubmenu(index)"
+            @mouseleave="resetHighlight"
+          >
+            {{ submenu.title }}
+            <div
+              class="nested-dropdown-content"
+              v-if="submenu.submenus && submenu.submenus.length > 0"
+              :style="{ minWidth: nestedDropdownMinWidth + 'px' }"
+              @mouseover="showNestedDropdown(index)"
+              @mouseleave="hideNestedDropdown(index)"
+            >
+              <template v-for="(nestedSubmenu, nestedIndex) in submenu.submenus" :key="nestedIndex">
+                <NuxtLink
+                  :to="nestedSubmenu.to"
+                  class="submenu-item"
+                >
+                  {{ nestedSubmenu.title }}
+                </NuxtLink>
+              </template>
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+  to: String,
+  target: String,
+  hasSubmenus: Boolean,
+  submenus: Array,
+  dropdownMinWidth: Number,
+  nestedDropdownMinWidth: Number
+});
+
+const buttonClasses = ref('text-gray-999 hover:text-black transition duration-300');
+const isDropdownVisible = ref(false);
+const highlightedSubmenu = ref(null);
+
+const showDropdown = () => {
+  if (props.hasSubmenus) {
+    isDropdownVisible.value = true;
+  }
+};
+
+const hideDropdown = () => {
+  isDropdownVisible.value = false;
+  resetHighlight();
+};
+
+const highlightSubmenu = (index) => {
+  highlightedSubmenu.value = index;
+};
+
+const resetHighlight = () => {
+  highlightedSubmenu.value = null;
+};
+
+const showNestedDropdown = (index) => {
+  highlightedSubmenu.value = index;
+};
+
+const hideNestedDropdown = (index) => {
+  if (highlightedSubmenu.value === index) {
+    highlightedSubmenu.value = null;
+  }
+};
+</script>
+
+<style scoped>
+.no-border {
+  border: none;
+}
+
+.relative {
+  position: relative;
+}
+
+.button-content {
+  display: flex;
+  align-items: left;
+}
+
+.caret {
+  margin-left: 5px;
+  margin-top: -5px;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  font-size: 12px;
+  text-align: justify;
+  background-color: #f1f1f1;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  left: 0;
+  top: 100%;
+  border: 1px solid #ccc;
+}
+
+.nested-dropdown-content {
+  display: none;
+  position: absolute;
+  font-size: 12px;
+  text-align: justify;
+  background-color: #f1f1f1;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  left: 100%;
+  top: 0;
+  border: 1px solid #ccc;
+}
+
+.dropdown-content > * {
+  padding: 12px 16px;
+  cursor: pointer;
+  display: block;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.dropdown-content > *.submenu-item {
+  color: #333;
+}
+
+.dropdown-content > *.submenu-item:hover {
+  background-color: #d3d3d3;
+  color: rgb(70, 184, 67);
+}
+
+.dropdown-content > *:not(:last-child) {
+  border-bottom: 1px solid #ccc;
+}
+
+.relative:hover .dropdown-content {
+  display: block;
+}
+
+.submenu-item:hover .nested-dropdown-content {
+  display: block;
+}
+
+.highlighted {
+  background-color: #333;
+  color: rgb(70, 184, 67);
+}
+
+.button-content::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 55%;
+  transform: translateY(-50%);
+  width: 2px;
+  height: 55%;
+  background-color: transparent;
+  transition: background-color 0.3s;
+}
+
+.no-border:hover .button-content::before,
+.active .button-content::before {
+  background-color: rgb(70, 184, 67);
+}
+</style>
