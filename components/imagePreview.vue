48476a8a7bd6b8d6d20105e6062aa3dcbc6b6eac
Ofek Shaltiel <72337187+OfekShaltiel-UTD@users.noreply.github.com>
2024-08-07 03:14:36 -0500

    Fix for the hotfix

--- components/imagePreview.vue
+++ components/imagePreview.vue
@@ -75,8 +75,8 @@ const saveImage = async (theImage: Image) => {
     div(v-if="images.length !=0" style='position: relative;') 
         img.cursor-pointer.object-cover.align-middle.rounded-lg(class="w-40 sm:w-64" :src = "previewImage?.url")
         .absolute(style='top: 10px; right: 150px')
-            button.bg-red-500(class='w-40 sm:64' style="align-items: center;justify-content: center; line-height: 1;text-align: center; color: white; font-weight: 450; positon: absolute; top:0px; left: 0px; width: 30px; height: 2rem; border-radius: 50%; padding-bottom: 4px;" @click = "removeImage(previewCuid, true)") x
-    a.ml-10.pt-1(style="text-shadow: 3px 3px 4px rgba(0, 0, 0, 0.25); background-color:red;") Image Upload
+            button.bg-red-500(class='w-40 sm:64' style="align-items: center;justify-content: center; line-height: 1;text-align: center; color: white; font-weight: 450; positon: absolute; top:0px; left: 0px; width: 30px; height: 2rem; border-radius: 50%; padding-bottom: 4px; background-color: red;" @click = "removeImage(previewCuid, true)") x
+    a.ml-10.pt-1(style="text-shadow: 3px 3px 4px rgba(0, 0, 0, 0.25);") Image Upload
         ImageUpload(@imageUploaded="saveImage" :pageCuid="props.pageCuid")
 .py-4.grid.flex-box.flex-row.item-centered.gap-1(v-if="images.length!= 0" class="sm:grid-cols-3" style="line-height: 0px;text-align: center")
     div(style="width:1200px" class="")
