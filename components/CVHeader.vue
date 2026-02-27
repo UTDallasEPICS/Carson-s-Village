5156c8fcd2dd974ab7ac501d2477097b00f92504
Ofek Shaltiel <ofekshaltieldev@gmail.com>
2026-02-26 20:56:28 -0600

    Fix code style on header and fix button alignment

--- components/CVHeader.vue
+++ components/CVHeader.vue
@@ -31,63 +31,62 @@ const handleHamburgerClick = () => {
 
 <template lang="pug">
 ClientOnly
-  .max-w-min.mx-auto.pt-3.flex.gap-2.mt-7.text-center
-    div.max-w-min.mx-auto.flex.gap-2(v-if="!isLoggedIn")
-      a.w-20.items-center.px-2.py-2.text-sm.font-medium.rounded-md.text-blue-999.cursor-pointer(
-        class='hover:text-black bg-white'
-        target="blank"  
+  div(class="max-w-min mx-auto pt-3 flex items-center gap-2 mt-7 text-center")
+    div(class="max-w-min mx-auto flex items-center gap-2" v-if="isLoggedIn")
+      a(class="w-20 flex items-center px-2 py-2 text-sm font-medium rounded-md text-blue-999 cursor-pointer hover:text-black bg-white"
+        target="blank"
         href="https://carsonsvillage.org"
-      )         
-        img.w-20.h-14(src="/CVLogo.png")
-
-      a.items-center.pt-5.px-2.py-2.text-sm.font-medium.rounded-md.text-blue-999.cursor-pointer(
-        class='hover:text-black bg-white'  
+      )
+        img(class="w-20 h-14" src="/CVLogo.png")
+      a(class="flex items-center pt-5 px-2 py-2 text-sm font-medium rounded-md text-blue-999 cursor-pointer hover:text-black bg-white"
         href="/api/logout"
-      ) 
-        p.uppercase.white.w-max.font-bold.text-orange-999 LOGOUT
+      )
+        p(class="uppercase white w-max font-bold text-orange-999") LOGOUT
       DropdownMenu(:has-submenus="true" :num-submenus="4"
         :submenus="[{ title: 'Timeline of Important Events', to: 'https://carsonsvillage.org/timeline-of-events/' }, { title: 'Resource Library', to: 'http://carsonsvillage.org/resource-library' }, { title: 'Group Support', to: 'https://carsonsvillage.org/grief-group-support/' }, { title: 'Find Support', to: 'https://carsonsvillage.org/support/' }]" :dropdownMinWidth="200")
         | RESOURCES
       NavLinkButton(to='https://carsonsvillage.org/get-involved/' target="_blank")
-        p.uppercase.white.w-max.text-black-999 GET INVOLVED
+        p(class="uppercase white w-max text-black-999") GET INVOLVED
       DropdownMenu(:has-submenus="true" :num-submenus="6"
         :submenus="[{ title: 'Our Story', to: 'https://carsonsvillage.org/about-us/our-family/' }, { title: 'Our Testimonies', to: 'https://carsonsvillage.org/our-testimonials/' }, { title: 'In The News', to: 'https://carsonsvillage.org/about-us/in-the-news/' }, { title: 'Newsletter Archive', to: 'https://carsonsvillage.org/about-us/newsletter-archive/' }, { title: 'Our Team >', submenus: [{ title: 'Advocates', to: 'https://carsonsvillage.org/about-us/advocates/' }, { title: 'Clinical Consultants', to: 'https://carsonsvillage.org/about-us/clinical-consultants/' }, { title: 'Support Team', to: 'https://carsonsvillage.org/about-us/meet-our-team/' }]}, { title: 'Board of Directors', to: 'https://carsonsvillage.org/about-us/board-of-directors/' }]" :dropdownMinWidth="150" :nestedDropdownMinWidth="150")
         | ABOUT&nbsp;US
-      button.items-center.px-2.py-2.text-sm.rounded-md.cursor-pointer(
-        class='hover:text-black bg-white lg:hidden'  
+      button(class="flex items-center px-2 py-2 text-sm font-medium rounded-md text-blue-999 cursor-pointer hover:text-black bg-white lg:hidden mt-3"
         @click="handleHamburgerClick"
-      ) 
-        p.uppercase.white.w-max.font-bold |||
-    div.max-w-min.mx-auto.flex.gap-2(v-else)
-      a.w-20.items-center.px-2.py-2.text-sm.font-medium.rounded-md.text-blue-999.cursor-pointer(
-        class='hover:text-black bg-white'
-        target="blank"  
+      )
+        p(class="uppercase white w-max font-bold") |||
+
+    div(class="max-w-min mx-auto flex items-center gap-2" v-else)
+      a(class="w-20 flex items-center px-2 py-2 text-sm font-medium rounded-md text-blue-999 cursor-pointer hover:text-black bg-white"
+        target="blank"
         href="https://carsonsvillage.org"
-      )         
-        img.w-20.h-14(src="/CVLogo.png")
-
-      a.items-center.pt-5.px-2.py-2.text-sm.font-medium.rounded-md.text-blue-999.cursor-pointer(
-      class='hover:!text-black bg-white'  
-      href="/api/login"
-      ) 
-        p.uppercase.white.w-max.font-bold.text-orange-999 LOGIN
-
+      )
+        img(class="w-20 h-14" src="/CVLogo.png")
+      a(class="flex items-center pt-5 px-2 py-2 text-sm font-medium rounded-md text-blue-999 cursor-pointer hover:!text-black bg-white"
+        href="/api/login"
+      )
+        p(class="uppercase white w-max font-bold text-orange-999") LOGIN
       DropdownMenu(:has-submenus="true" :num-submenus="4"
         :submenus="[{ title: 'Timeline of Important Events', to: 'https://carsonsvillage.org/timeline-of-events/' }, { title: 'Resource Library', to: 'http://carsonsvillage.org/resource-library' }, { title: 'Group Support', to: 'https://carsonsvillage.org/grief-group-support/' }, { title: 'Find Support', to: 'https://carsonsvillage.org/support/' }]" :dropdownMinWidth="200")
         | RESOURCES
       NavLinkButton(to='https://carsonsvillage.org/get-involved/' target="_blank")
-        p.pl-2.uppercase.white.w-max.text-black-999 GET INVOLVED
-      
+        p(class="pl-2 uppercase white w-max text-black-999") GET INVOLVED
       DropdownMenu(:has-submenus="true" :num-submenus="6"
         :submenus="[{ title: 'Our Story', to: 'https://carsonsvillage.org/about-us/our-family/' }, { title: 'Our Testimonies', to: 'https://carsonsvillage.org/our-testimonials/' }, { title: 'In The News', to: 'https://carsonsvillage.org/about-us/in-the-news/' }, { title: 'Newsletter Archive', to: 'https://carsonsvillage.org/about-us/newsletter-archive/' }, { title: 'Our Team >', submenus: [{ title: 'Advocates', to: 'https://carsonsvillage.org/about-us/advocates/' }, { title: 'Clinical Consultants', to: 'https://carsonsvillage.org/about-us/clinical-consultants/' }, { title: 'Support Team', to: 'https://carsonsvillage.org/about-us/meet-our-team/' }]}, { title: 'Board of Directors', to: 'https://carsonsvillage.org/about-us/board-of-directors/' }]" :dropdownMinWidth="150" :nestedDropdownMinWidth="150")
         | ABOUT&nbsp;US
-      button(@click="handleHamburgerClick" class="lg:hidden")
-        p.pl-2.uppercase.white.w-max.text-black-999 |||
-    .flex.w-max.px-2.pt-2.gap-2(v-if="isNotSearch")
-      input(id="search" class="border border-gray-300 py-2 px-4 rounded-lg focus:outline-none focus:border-black-500" 
-      type="search" placeholder=" " v-model="searchQuery" v-on:keyup.enter="onEnter" style="height: 40px")
-      NuxtLink.inline(:to="`/Search/?search=${searchQuery}&isPageList=0`")
-        img(src="/CVSearchIcon.png" style="height: 40px")
+      button(class="flex items-center px-2 text-sm font-medium rounded-md text-blue-999 cursor-pointer hover:text-black bg-white lg:hidden"
+        @click="handleHamburgerClick"
+      )
+        p(class="uppercase white w-max font-bold pl-2") |||
+
+    div(class="flex items-center w-max px-2 mt-3" v-if="isNotSearch")
+      input(class="border border-gray-300 py-2 px-4 rounded-lg focus:outline-none focus:border-black-500 h-10"
+        type="search"
+        placeholder=" "
+        v-model="searchQuery"
+        v-on:keyup.enter="onEnter"
+      )
+      NuxtLink(class="inline flex items-center" :to="`/Search/?search=${searchQuery}&isPageList=0`")
+        img(class="h-10" src="/CVSearchIcon.png")
 </template>
 
 <style scoped>
@@ -99,20 +98,4 @@ ClientOnly
   position: relative;
 }
 
-.button-content::before {
-  content: '';
-  position: absolute;
-  left: 0;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 2px;
-  height: 100%;
-  background-color: transparent;
-  transition: background-color 0.3s;
-}
-
-.no-border:hover .button-content::before,
-.active .button-content::before {
-  background-color: green;
-}
 </style>
