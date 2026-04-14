943beb2a4cdad4ff9d8a29204fea105e3627def6
beesoncaleb <caleb.f.beeson@gmail.com>
2026-04-13 19:38:58 -0500

    Updated prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
-import "dotenv/config"
-import { defineConfig, env } from "prisma/config"
+import 'dotenv/config'
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-    schema: "prisma/schema.prisma",
-    migrations: {
-        path: "prisma/migrations",
-    },
-    datasource: {
-        url: env("DATABASE_URL"),
-    },
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations'
+  },
+  datasource: {
+    url: process.env['DATABASE_URL'],
+  },
 })
