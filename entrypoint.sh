a09df0d5734fb3ef5501cd9d282c6c1b4f5acad1
beesoncaleb <caleb.f.beeson@gmail.com>
2026-04-13 19:57:25 -0500

    Added prisma seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Apply migrations and initialize migrations if it does not exist
 pnpm prisma generate
 pnpm prisma migrate deploy
+pnpm prisma db seed
 
 # Run the CMD command from the dockerfile
 exec "$@"
--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
-    path: 'prisma/migrations'
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts'
   },
   datasource: {
     url: process.env['DATABASE_URL'],
