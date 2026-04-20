3b4399f18393fdac9b8d54d5a3d568327a201389
beesoncaleb <caleb.f.beeson@gmail.com>
2026-04-19 19:11:19 -0500

    Fixed middelware breaking when not authenticated

--- app/middleware/family-guard.ts
+++ app/middleware/family-guard.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const { data: session } = await authClient.useSession(useFetch);
 
-  if (!session.value && session.value.user.role !== 'admin' && session.value.user.role !== 'advocate' && session.value.user.role !== 'family') {
+  if (!session.value && session.value?.user.role !== 'admin' && session.value?.user.role !== 'advocate' && session.value?.user.role !== 'family') {
     return navigateTo('/Search/?search=')
   }
 })
