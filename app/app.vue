5767f384efb38b6b2ef494d375426a2d232821b5
beesoncaleb <caleb.f.beeson@gmail.com>
2026-08-31 21:01:03 -0500

    feat: stripe onboarding has been moved to a warning and link
    Stripe onboarding flow used to start automatically after login, this is jarring and confusing to a new user
    
    Now users are prompted with a warning about stripe onboarding and click a link to begin onboarding

--- app/app.vue
+++ app/app.vue
@@ -3,6 +3,7 @@
     <VerticalNav
       v-if="session"
       class="hidden lg:block"
+      :requireOnboarding="requireOnboarding"
     />
     <div class="flex flex-col gap-5 min-h-screen grow">
       <CVHeader/>
@@ -16,4 +17,20 @@
 import { authClient } from '~/utils/auth-client';
 const { data: session } = await authClient.useSession(useFetch);
 
+// Flag users that need stripe onboarding
+const requireOnboarding = ref(false)
+const { data: family, error: familyError } = await useFetch(() => `/api/family/${session.value?.user?.familyId}`, {
+  method: 'GET',
+  immediate: !!session.value?.user?.familyId 
+});
+
+// We need a watchEffect (or computed) here because 'family' might not be populated immediately 
+// if the fetch is delayed or reactive.
+watchEffect(() => {
+  if (!familyError.value && family.value?.acceptingDonations && !family.value?.stripe_account_id) {
+    requireOnboarding.value = true;
+  } else {
+    requireOnboarding.value = false;
+  }
+});
 </script>
